b437ec68c8fb0553dbf9fde7d56c09565b8a3a15
Ryan Clark <ryan.clark.j@gmail.com>
2013-01-15 11:41:06 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-ngds',
+	version=version,
+	description="Extension for NGDS-related customizations",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='',
+	author_email='',
+	url='',
+	license='',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.ngds'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.ngds:PluginClass
+	""",
+)
