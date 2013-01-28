bac1459ebb05d840cdec6eed64691def48e2ff9d
Ryan Clark <ryan.clark.j@gmail.com>
2013-01-28 15:45:56 -0700

    Added API controllers for harvest tables

--- ckanext/ngds/harvest/plugin.py
+++ ckanext/ngds/harvest/plugin.py
@@ -1,6 +1,7 @@
 from ckan.plugins import implements, SingletonPlugin
-from ckan.plugins import IDatasetForm, IConfigurer, IActions
+from ckan.plugins import IConfigurer, IActions
 from ckanext.ngds.harvest.model.harvest_node import define_tables
+from ckanext.ngds.harvest.controllers.ngds_harvest import dispatch
 
 class NgdsHarvestPlugin(SingletonPlugin):
     """Control harvesting operations"""
@@ -11,3 +12,11 @@ class NgdsHarvestPlugin(SingletonPlugin):
         """IConfigurable function. config is a dictionary of configuration parameters"""
         # Provides a point to do mappings from classes to database tables whenever CKAN is run
         define_tables()
+        
+    implements(IActions) # Allows us to build a URL and associated binding to a python function
+    
+    def get_actions(self):
+        """IActions function. Should return a dict keys = function name and URL, value is the function to execute"""
+        return {
+            "ngds_harvest": dispatch
+        }
