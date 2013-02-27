694c9bad902387ff7dc31ef6c42768e88155d495
Ryan Clark <ryan.clark.j@gmail.com>
2013-02-27 11:07:14 -0700

    Remove calls to old iso_support.py. This file will be replaced with the IsoPackage class

--- ckanext/ngds/csw/plugin.py
+++ ckanext/ngds/csw/plugin.py
@@ -1,7 +1,6 @@
 from ckan.plugins import implements, SingletonPlugin
 from ckan.plugins import IConfigurer, IRoutes
 from ckanext.ngds.csw.model.csw_records import define_tables
-from ckanext.ngds.csw.iso_support import add_csw_model
 
 class CswPlugin(SingletonPlugin):
     """The purpose of this plugin is to add CSW support"""
@@ -12,9 +11,6 @@ class CswPlugin(SingletonPlugin):
         """IConfigurable function. config is a dictionary of configuration parameters"""
         # Provides a point to do mappings from classes to database tables whenever CKAN is run
         define_tables()
-        
-        # Add CswPackage class for conversion to/from ISO19139 XML
-        add_csw_model()
         
     implements(IRoutes) # Allows me to add URLs to the CKAN site
     
