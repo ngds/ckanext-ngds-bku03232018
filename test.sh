a7d27545fbf9edc97af3bb70837396ceac21ae97
Genhan <genhan.chen@azgs.az.gov>
2013-03-12 22:26:33 +0000

    Updated test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ sudo su postgres -c "psql -c 'CREATE DATABASE $db OWNER $dbuser'"
 
 #Run nose tests
 cd ~/pyenv/src/ckanext-ngds
-nosetests --ckan --with-pylons=test-core.ini
+~/pyenv/bin/nosetests --ckan --with-pylons=test-core.ini
 
 #Drop the test database
 sudo su postgres -c "psql -c 'DROP DATABASE $db'" 
