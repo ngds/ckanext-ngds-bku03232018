81adaed277df4db0836989574f84c39bd74b3f2a
viveksunder <sviveksunder@gmail.com>
2013-03-25 15:18:55 -0400

    Getting rid of print statements

--- ckanext/ngds/ngdsui/plugin.py
+++ ckanext/ngds/ngdsui/plugin.py
@@ -133,18 +133,15 @@ class NgdsuiPlugin(SingletonPlugin):
 			search_params['fq'] = ''
 
 		search_params['fq'] = search_params['fq']+' capacity:"public"'
-		print "close to in"
 		if 'extras' in search_params and 'poly' in search_params['extras'] and search_params['extras']['poly']:
 			# do some validation
 	        # We'll perform the existing search but also filtering by the ids
             # of datasets within the bbox
 			x =  { 'poly':search_params['extras']['poly'] }
-			print "in"
 			bbox_query_ids = get_package_ids_in_poly(x,'4326')
 			q = search_params.get('q','')
 			new_q = '%s AND ' % q if q else ''
 			new_q += '(%s)' % ' OR '.join(['id:%s' % id for id in bbox_query_ids])
-			print new_q
 			search_params['q'] = new_q
 		else:
 			print "Definitely not in"
