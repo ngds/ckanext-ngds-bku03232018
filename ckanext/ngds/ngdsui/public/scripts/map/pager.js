cf6d7651f55834f22a72f8e209c7168936db6ee0
Vivek Sunder <sviveksunder@gmail.com>
2013-04-03 00:36:15 -0400

    Fixing url bug

--- ckanext/ngds/ngdsui/public/scripts/map/pager.js
+++ ckanext/ngds/ngdsui/public/scripts/map/pager.js
@@ -115,7 +115,7 @@ ngds.Pager = function(rows) {
 			inc = 1;
 			$(".wms").click(function(ev){
 				var id=ev.currentTarget.id;
-						var ngds_layer = L.tileLayer.wms("http://localhost:8080/geoserver/NGDS/wms",{
+						var ngds_layer = L.tileLayer.wms("/geoserver/NGDS/wms",{
 						layers:"NGDS:"+id,
 						format: 'image/png',
 					    transparent: true,
