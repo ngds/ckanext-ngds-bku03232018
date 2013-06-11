2219aa6ebef01dff6da67520f54c0e0e52368ba1
Ryan Clark <ryan.clark.j@gmail.com>
2013-06-10 17:26:54 -0700

    encorporate more content into XML output

--- ckanext/ngds/csw/logic/view.py
+++ ckanext/ngds/csw/logic/view.py
@@ -2,6 +2,7 @@ from ckan.plugins import toolkit
 from ckanext.harvest.model import HarvestObject
 from shapely.geometry import asShape
 import json
+from dateutil import parser as date_parser
 
 def iso_metadata(context, data_dict):
     """
@@ -19,20 +20,37 @@ def iso_metadata(context, data_dict):
     p = toolkit.get_action("package_show")(context, {"id": package_id})
 
     # This is lifted from ckanext-harvest plugin.py:before_view. Looks for a HarvestObject to go with your package
-    harvest_object = context["model"].Session.query(HarvestObject) \
+    '''harvest_object = context["model"].Session.query(HarvestObject) \
                     .filter(HarvestObject.package_id==p["id"]) \
                     .filter(HarvestObject.current==True) \
-                    .first()
+                    .first()'''
+    harvest_object = None
+
     # NOTE: If this is a harvested package, we should just return the XML, without alteration
     if harvest_object:
         output = harvest_object.content
 
     else:
-        # Extract BBOX coords from extras
+        # ---- Reformat extras so they can be looked up
+        p["additional"] = {}
+        for extra in p["extras"]:
+            p["additional"][extra["key"]] = extra["value"]
+
+        # ---- Remove milliseconds from metadata dates
+        p["metadata_modified"] = date_parser.parse(p.get("metadata_modified", "")).replace(microsecond=0).isoformat()
+        p["metadata_created"] = date_parser.parse(p.get("metadata_created", "")).replace(microsecond=0).isoformat()
+
+        # ---- Figure out URIs
+        other_ids = p["additional"].get("other_id", [])
+        if len(other_ids) > 0:
+            p["additional"]["datasetUri"] = other_ids[0]
+        else:
+            p["additional"]["datasetUri"] = "this should be the CKAN URL"
+
+        # ---- Extract BBOX coords from extras
         p["extent"] = {}
 
-        # stupid python logic to try and get the first item or None from a list
-        geojson = next(iter([k["value"] for k in p["extras"] if k["key"] == "spatial"]), None)
+        geojson = p["additional"].get("spatial", None)
 
         if geojson is not None:
             try:
@@ -47,6 +65,40 @@ def iso_metadata(context, data_dict):
                 # Couldn't parse spatial extra into bounding coordinates
                 pass
 
+        # ---- Reformat resource extras
+        for resource in p.get("resources", []):
+            resource["additional"] = {}
+            for e in resource.get("extras", []):
+                resource["additional"][e["key"]] = e["value"]
+
+        # ---- Reorganize resources by distributor, on/offline
+        online = {}
+        offline = {}
+        for resource in p.get("resources", []):
+            distributor = resource["additional"].get("distributor", None)
+
+            if resource["additional"].get("is_online", True):
+                resources = online
+            else:
+                resources = offline
+
+            if distributor:
+                name = distributor.get("distributor_name", "None")
+            else:
+                name = "None"
+
+            if name not in resources.keys():
+                resources[name] = {
+                    "distributor": distributor,
+                    "resources": [resource]
+                }
+            else:
+                resources[name]["resources"].append(resource)
+
+        p["additional"]["online"] = [value for key, value in online.iteritems()]
+        p["additional"]["offline"] = [value for key, value in offline.iteritems()]
+
+        # ---- All done, render the template
         output = toolkit.render("package_to_iso.xml", p)
 
     return output
