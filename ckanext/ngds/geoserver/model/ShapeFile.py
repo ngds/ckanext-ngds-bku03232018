226f13be77038913a623adff198c0e342ec192d3
Ryan Clark <ryan.clark.j@gmail.com>
2013-06-17 12:12:13 -0700

    starting shapefile model

--- ckanext/ngds/geoserver/model/ShapeFile.py
+++ ckanext/ngds/geoserver/model/ShapeFile.py
@@ -1,8 +1,27 @@
+from ckanext.ngds.geoserver.model.Data import Data
+from ckan.plugins import toolkit
 
 class Shapefile(Data):
+    resource = None
 
-	def publish():
+    def __init__(self, resource_id):
+        self.resource = toolkit.get_action("resource_show")(None, {"id": resource_id})
+        self.zip_file = ""
+
+        
+        pass
+
+    def unzip(self):
+        pass
+
+    def reproject(self):
+        pass
+
+    def validate(self):
+        pass
+
+	def publish(self):
 		pass
 
-	def unpublish():
+	def unpublish(self):
 		pass
