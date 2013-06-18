c2271b147147af42c1c25ccd577d10b28b60ea15
Vivek Sunder <sviveksunder@gmail.com>
2013-06-18 09:21:44 -0700

    Publish for datastore

--- ckanext/ngds/geoserver/model/Datastored.py
+++ ckanext/ngds/geoserver/model/Datastored.py
@@ -1,13 +1,7 @@
-from ckan import model
-from ckan.lib.base import (request,
-                           render,
-                           model,
-                           abort, h, g, c)
-import ckanext.datastore.logic.action as datastore
-from ckan.model import Session
 import pylons
 from pylons import config
 import ckanext.datastore.db as db
+from ckan.logic import get_action
 
 class Datastored(object):
 	resource_id = None
@@ -26,23 +20,29 @@ class Datastored(object):
 			raise ValueError("Expected datastore write url to be configured in development.ini")
 
 	def publish(self):
-		context = { 'model':model,'session':Session, 'user':c.user }
-		datastore_exists= datastore.datastore_search(context,{ 'resource_id':self.resource_id,'limit':1 })>0
+		datastore_exists= get_action('datastore_search')(None,{ 'resource_id':self.resource_id,'limit':1 })>0
 		
 		if not datastore_exists:
 			return False
 
 		conn_params = { 'connection_url':self.connection_url,'resource_id':self.resource_id }
+
 		engine = db._get_engine(None, conn_params)
-		context['connection'] = engine.connect()
+		context = { 'connection':engine.connect() }
 		fields = db._get_fields(context,conn_params)
 
-		if self.geo_col not in fields:
+		if True in { col['id'] == self.geo_col for col in fields }:
+			return True
+		else:
 			fields.append({'id': self.geo_col, 'type': u'geometry' })
 			trans = context['connection'].begin()
-			new_column_res = context['connection'].execute(
-						"SELECT AddGeometryColumn('public', '"+self.resource_id+
-						"', '"+ self.geo_col+"', 4326, 'GEOMETRY', 2)")
+			try:
+				new_column_res = context['connection'].execute(
+							"SELECT AddGeometryColumn('public', '"+self.resource_id+
+							"', '"+ self.geo_col+"', 4326, 'GEOMETRY', 2)")
+			except RuntimeError:
+				pass
+
 			trans.commit()
 			return True
 
