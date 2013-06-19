a198dbefbf7d184a475b7753fc88546ffb6dfae6
Prahadeesh <prahad@gmail.com>
2013-06-19 17:42:55 -0400

    updated for geoserver

--- ckanext/ngds/geoserver/misc/helpers.py
+++ ckanext/ngds/geoserver/misc/helpers.py
@@ -1,15 +1,10 @@
-from ckan.lib.base import model,h,g,c,request
-import ckanext.ngds.geoserver.logic.action as geoserver_actions
-import ckan.logic as logic
-NotFound = logic.NotFound
-from pylons import config
 
-def is_spatialized(res_id,col_geo):
-	context = {'model': model, 'session': model.Session,\
-                   'user': c.user or c.author, 'for_view': True}
-	try:
-		is_spatialized = geoserver_actions.datastore_is_exposed_as_layer(context,{'id':res_id})['is_exposed_as_layer']
-		print is_spatialized
-	except(NotFound):
-		is_spatialized = False
-	return is_spatialized
+def is_spatialized(resource):
+    is_spatialized = False
+    try:
+        if resource.get('layer_name') and resource.get('layer_name') != 'None':
+            is_spatialized = True
+        print is_spatialized
+    except Exception as ex:
+        is_spatialized = False
+    return is_spatialized
