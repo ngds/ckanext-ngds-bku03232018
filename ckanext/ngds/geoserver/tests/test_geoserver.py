c3da1b8ef5ac1ddc8f952aedfa019ad9f4150d24
Ryan Clark <ryan.clark.j@gmail.com>
2013-06-28 15:38:12 -0700

    added logic for generating packages into NgdsTestCase

--- ckanext/ngds/geoserver/tests/test_geoserver.py
+++ ckanext/ngds/geoserver/tests/test_geoserver.py
@@ -1,9 +1,7 @@
 from ckanext.ngds.tests.ngds_test_case import NgdsTestCase
+from ckanext.ngds.geoserver.model.Geoserver import Geoserver
 
 class TestGeoserverConnector(NgdsTestCase):
-    def setUp(self):
-        self.ngds_table_set_up()
-
     def test_spatial_tables_exist(self):
         from sqlalchemy import create_engine
         from pylons import config
@@ -11,3 +9,17 @@ class TestGeoserverConnector(NgdsTestCase):
         engine = create_engine(config.get("sqlalchemy.url"))
         self.assertTrue(engine.dialect.has_table(engine.connect(), "geometry_columns"))
 
+    def test_geoserver_connection(self):
+        """Geoserver class method should return a Geoserver instance"""
+        gs = Geoserver.from_ckan_config()
+        self.assertIsInstance(gs, Geoserver)
+
+    def test_get_default_workspace(self):
+        """Geoserver must be able to create or return the default workspace"""
+        gs = Geoserver.from_ckan_config()
+        ws = gs.default_workspace()
+        self.assertEqual(ws.name, "ngds") # should read from config
+
+    def test_package_generator(self):
+        p = self.add_package("test-package")
+        self.assertIsNotNone(p)
--- ckanext/ngds/tests/ngds_test_case.py
+++ ckanext/ngds/tests/ngds_test_case.py
@@ -1,7 +1,12 @@
 from unittest import TestCase
 from postgis_sql import POSTGIS, SPATIAL_REF_SYS
+from ckanext.ngds.env import ckan_model as model
+from ckan.plugins import toolkit
 
 class NgdsTestCase(TestCase):
+    _sys_admin = None
+    _public_org = None
+
     @classmethod
     def setUpClass(cls):
         from sqlalchemy.orm import sessionmaker
@@ -23,6 +28,48 @@ class NgdsTestCase(TestCase):
             print "TABLE GENERATION FAILED: %s" % ex.orig
 
         session.close()
+
+        # Build plugin tables
+        from ckanext.harvest.model import setup as harvest_tables
+        harvest_tables()
+
+    def admin_user(self):
+        """Find/Create an admin user"""
+        if not self._sys_admin:
+            sys_admin = model.User.get('test_sysadmin')
+            if not sys_admin:
+                sys_admin = model.User(name='test_sysadmin', sysadmin=True)
+                model.Session.add(sys_admin)
+                model.Session.commit()
+                model.Session.remove()
+            self._sys_admin = sys_admin
+        return self._sys_admin
+
+    def public_org(self):
+        if not self._public_org:
+            org = dict(
+                name="public",
+                users=[{"name": self.admin_user().name}]
+            )
+            self._public_org = toolkit.get_action("organization_create")({"user": self.admin_user().name}, org)
+        return self._public_org
+
+    def add_package(self, package_name, package_dict={}):
+        package = dict(
+            name=package_name,
+            title="Test title",
+            notes="Test notes",
+            owner_org=self.public_org()["name"],
+            private=False,
+            state="active",
+            resources=[]
+        )
+
+        package.update(package_dict)
+        return toolkit.get_action("package_create")({"user": self.admin_user().name}, package)
+
+    def add_resource(self, resource_dict):
+        pass
 
     def ngds_table_set_up(self):
         pass
