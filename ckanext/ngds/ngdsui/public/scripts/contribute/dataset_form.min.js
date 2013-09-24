0faeab035f7244b045c775ab8b17a730d019b160
viveksunder <sviveksunder@gmail.com>
2013-09-24 10:29:45 -0400

    Some deletions

--- ckanext/ngds/ngdsui/public/scripts/contribute/dataset_form.min.js
+++
@@ -1,1 +0,0 @@
-$("[name=title]").on("input",function(a){var b=$("[name=title]").val();b=b.trim();b=b.replace(/^-/,"");b=b.replace(/-$/,"");b=b.replace(/[^a-zA-Z0-9]/g,"-");b=b.replace(/-{2,}/g,"-");$("[name=name]").val(b.toLowerCase())});
