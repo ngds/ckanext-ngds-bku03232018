31b9115700d3214b8c2eda6b42c8a72d498a20c0
asonnenschein <adrian.sonnenschein@gmail.com>
2013-12-23 12:51:47 -0700

    setting up env and start refactoring

---
+++ ckanext/ngds/ngdsui/public/scripts/map/a-ngds-map.js
@@ -0,0 +1,23 @@
+/**
+ * Created by adrian on 12/23/13.
+ */
+
+ngds.Map = {
+    map: L.map('map-container', {center: [39.977, -97.646], zoom: 4,
+        maxBounds: [[-84, -179], [84, 179]], attributionControl: true}),
+    layers: {
+        baseLayer: L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg', {
+            subdomains: '1234',
+            attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; ' +
+                '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.' +
+                'org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+            detectRetina: true
+        })
+    },
+    controls: {
+        loading: L.Control.loading({separate: true, position: 'topleft'})
+    }
+};
+
+ngds.Map.layers.baseLayer.addTo(ngds.Map.map);
+ngds.Map.map.addControl(ngds.Map.controls.loading);
