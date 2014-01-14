9c2f27ed4949b7cf1e3ccf504d3b22fd7b83ae44
asonnenschein <adrian.sonnenschein@gmail.com>
2014-01-14 12:46:11 -0700

    fixed dataset entry map problems

--- ckanext/ngds/ngdsui/public/scripts/dataset_entry_map.js
+++ ckanext/ngds/ngdsui/public/scripts/dataset_entry_map.js
@@ -9,65 +9,24 @@ $(document).ready(function () {
             'org/licenses/by-sa/2.0/">CC-BY-SA</a>',
         detectRetina: true
     });
-    map = new L.Map('bbox-map', {layers: [base], center: new L.LatLng(34.1618, -111.53332), zoom: 5});
+    var map = new L.Map('bbox-map', {layers: [base], center: new L.LatLng(34.1618, -111.53332), zoom: 5});
 
-   L.Clear = L.Control.extend({
-       options: {
-           position: 'topleft',
-           title: 'Clear Rectangle',
-           forceSeparateButton: false
-       },
-
-       onAdd: function (map) {
-           var container = L.DomUtil.create('div', 'leaflet-control-rectangle-clear-container');
-           this._createButton(this.options.title, 'leaflet-control-rectangle-clear', container, this.clearRectangle, map);
-           return container;
-       },
-
-       _createButton: function (title, className, container, fn, context) {
-           var link = L.DomUtil.create('a', className, container);
-           link.href = '#';
-           link.title = title;
-           L.DomEvent
-               .addListener(link, 'click', L.DomEvent.stopPropagation)
-               .addListener(link, 'click', L.DomEvent.preventDefault)
-               .addListener(link, 'click', fn, context);
-           return link;
-       },
-       clearRectangle: function () {
-           try {
-               if (map.hasLayer(map.originalExtent)) {
-                   map.removeLayer(map.originalExtent);
-               }
-           } catch(err) {
-               console.log("No original dataset extent layer.  Error: " + err);
-           }
-       }
-   });
-
-// Add the control panel
-    var drawControl = new L.Control.Draw({
-        position: 'topright',
-        polyline: false,
-        circle: false,
-        polygon: false,
-        marker: false
-    });
-    map.addControl(drawControl);
-
-    var clearControl = new L.Clear({
-        position: 'topright'
-    });
-    map.addControl(clearControl);
-
-// Add callbacks for when drawing is completed
-    var drawnItems = new L.LayerGroup();
+    var drawnItems = new L.FeatureGroup();
     map.addLayer(drawnItems);
 
-    map.on('draw:rectangle-created', function (e) {
+    var drawControl = new L.Control.Draw({
+        position: 'topright',
+        draw: {polyline: false, circle: false, marker: false, polygon: false},
+        edit: {featureGroup: drawnItems, remove: true, edit: false}
+    });
+
+    map.addControl(drawControl);
+
+    map.on('draw:created', function (e) {
+        var layer = e.layer;
         drawnItems.clearLayers();
-        drawnItems.addLayer(e.rect);
-        var geojson = JSON.stringify(e.rect.toGeoJSON().geometry);
+        drawnItems.addLayer(layer);
+        var geojson = JSON.stringify(layer.toGeoJSON().geometry);
         $('#field-extras-10-value').val(geojson);
     });
 });
