6f18a4f94a3ee6c49563f2d76def429518a2247e
asonnenschein <adrian.sonnenschein@gmail.com>
2014-07-28 09:33:20 -0700

    plugin interface for IAdminController

--- ckanext/ngds/common.py
+++ ckanext/ngds/common.py
@@ -3,3 +3,12 @@
 # file exists as a central place to organize these kinds of dependencies, so
 # that we can easily update our code in the future as changes are made in the
 # trunk branch of CKAN
+
+# Currently using all of these imports for the AdminController
+from pylons import config
+import ckan.lib.base as base
+import ckan.lib.helpers as h
+import ckan.lib.app_globals as app_globals
+import ckan.model as model
+import ckan.logic as logic
+import ckan.new_authz
