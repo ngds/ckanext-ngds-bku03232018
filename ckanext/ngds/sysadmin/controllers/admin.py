c1acffd6f370275719292c23653540e57a059a79
asonnenschein <adrian.sonnenschein@gmail.com>
2014-07-28 16:11:17 -0700

    settled on a layout and comments

--- ckanext/ngds/sysadmin/controllers/admin.py
+++ ckanext/ngds/sysadmin/controllers/admin.py
@@ -3,13 +3,26 @@ from ckanext.ngds.common import base, admin, app_globals, config, h
 request = base.request
 _ = base._
 
+# Register ngds admin configurations with pylons
 app_globals.mappings['ngds.publish'] = 'ngds.publish'
 app_globals.mappings['ngds.harvest'] = 'ngds.harvest'
 app_globals.mappings['ngds.edit_metadata'] = 'ngds.edit_metadata'
 
 class NGDSAdminController(admin.AdminController):
-
+    """
+    Vanilla CKAN provides no working interface for extending it's admin
+    functionality, so we have to inherit the AdminController object directly in
+    order to extend object.  Then we redirect the routes in the plugin.py file
+    to use this controller.
+    """
     def get_style_config_form_items(self):
+        """
+        Just a copy of the AdminController's _get_config_form_items function
+        that we rename so that we can build and pass other config form item
+        objects into the config method.
+
+        @return: dictionary
+        """
         styles = [{'text': 'Default', 'value': '/base/css/main.css'},
                   {'text': 'Red', 'value': '/base/css/red.css'},
                   {'text': 'Green', 'value': '/base/css/green.css'},
@@ -32,6 +45,12 @@ class NGDSAdminController(admin.AdminController):
         return items
 
     def get_ngds_config_form_items(self):
+        """
+        Custom config form controls for NGDS data contribution and interaction.
+
+        @return: dictionary
+        """
+
         data_controls = [{'text': 'Enabled', 'value': 'True'},
                        {'text': 'Disabled', 'value': 'False'}]
 
@@ -41,7 +60,15 @@ class NGDSAdminController(admin.AdminController):
         ]
         return items
 
-    def config(self, items, html_file):
+    def config(self, items):
+        """
+        Renders global config items based on user input in the UI.  Returns a
+        dictionary of config parameters that are used to build the admin config
+        Jinja templates.
+
+        @param items: pylons global config options
+        @return: dictionary
+        """
         data = request.POST
 
         if 'save' in data:
@@ -56,15 +83,27 @@ class NGDSAdminController(admin.AdminController):
         for item in items:
             name = item['name']
             data[name] = config.get(name)
-        vars = {'data': data, 'errors': {}, 'items': items}
 
-        return base.render('admin/' + html_file + '.html',
-                           extra_vars = vars)
+        return {'data': data, 'errors': {}, 'form_items': items}
 
     def style_config(self):
+        """
+        Render the admin config page for style and page layout.
+
+        @return: web page
+        """
         items = self.get_style_config_form_items()
-        self.config(items, 'style-config')
+        vars = self.config(items)
+        return base.render('admin/style-config.html',
+                           extra_vars=vars)
 
     def data_config(self):
+        """
+        Render the admin config page for NGDS data contribution and interaction.
+
+        @return: web page
+        """
         items = self.get_ngds_config_form_items()
-        self.config(items, 'data-config')
+        vars = self.config(items)
+        return base.render('admin/data-config.html',
+                           extra_vars=vars)
