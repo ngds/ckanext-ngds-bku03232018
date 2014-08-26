b379af2372e9fca8c2dc8b1862f63d6b86d4eb9c
asonnenschein <adrian.sonnenschein@gmail.com>
2014-08-26 11:32:41 -0700

    move mapsearch to its own repository

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
-""" NGDS_HEADER_BEGIN
-
-National Geothermal Data System - NGDS
-https://github.com/ngds
-
-File: <filename>
-
-Copyright (c) 2014, Siemens Corporate Technology and Arizona Geological Survey
-
-Please refer the the README.txt file in the base directory of the NGDS project:
-https://github.com/ngds/ckanext-ngds/blob/master/README.txt
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
-General Public License as published by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.  https://github.com/ngds/ckanext-ngds
-ngds/blob/master/LICENSE.md or
-http://www.gnu.org/licenses/agpl.html
-
-NGDS_HEADER_END """
-
 from setuptools import setup, find_packages
 
-version = '2.2.0'
+version = '2.0.0'
 
 setup(
 	name='ckanext-ngds',
@@ -35,8 +11,8 @@ setup(
 	classifiers=[],
 	keywords='',
 	author='Arizona Geological Survey',
-	author_email='',
-	url='http://geothermaldata.org',
+	author_email='adrian.sonnenschein@azgs.az.gov',
+	url='https://github.com/ngds/ckanext-ngds',
 	license='MIT',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
 	namespace_packages=['ckanext'],
@@ -55,15 +31,7 @@ setup(
     # NGDS Admin plugin
     ngds_sysadmin=ckanext.ngds.sysadmin.plugin:SystemAdministrator
 
-    # NGDS Map Search plugin
-    ngds_mapsearch=ckanext.ngds.mapsearch.plugin:MapSearch
-
 	# NGDS UI plugin
 	ngds_client=ckanext.ngds.client.plugin:NGDSClient
-
-	[paste.paster_command]
-    # Install NGDS additional tables
-	ngds=ckanext.ngds.base.commands.ngds_tables:NgdsTables
-	ngdsapi=ckanext.ngds.lib.command:APICommand
 	""",
 )
