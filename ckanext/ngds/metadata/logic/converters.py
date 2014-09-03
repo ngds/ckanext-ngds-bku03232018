c0697596c5b78db5093255eb2eb522ae71715cf1
asonnenschein <adrian.sonnenschein@gmail.com>
2014-09-02 19:52:11 -0700

    skeleton for metadata contrib backend

---
+++ ckanext/ngds/metadata/logic/converters.py
@@ -0,0 +1,20 @@
+def convert_to_authors(key, data, errors, context):
+    pass
+
+def convert_to_keywords(key, data, errors, context):
+    pass
+
+def convert_to_geo_extent(key, data, errors, context):
+    pass
+
+def convert_to_distributors(key, data, errors, context):
+    pass
+
+def convert_to_links(key, data, errors, context):
+    pass
+
+def convert_to_metadata_contact(key, data, errors, context):
+    pass
+
+def convert_to_harvest_info(key, data, errors, context):
+    pass
