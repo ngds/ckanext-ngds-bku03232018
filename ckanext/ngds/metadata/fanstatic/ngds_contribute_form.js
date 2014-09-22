00d396827d818f1d3dc5aae7e6ebe61c5f4f0667
asonnenschein <adrian.sonnenschein@gmail.com>
2014-09-22 16:58:05 -0700

    progress on js object for processing package metadata

--- ckanext/ngds/metadata/fanstatic/ngds_contribute_form.js
+++ ckanext/ngds/metadata/fanstatic/ngds_contribute_form.js
@@ -2,75 +2,208 @@
 
 /*
 Here's what we want the ngds package schema to look like:
-ngdsPackage: {
-  _id: 'undefined',
-  Title: 'undefined',
-  Description: 'undefined',
-  PublicationDate: 'undefined',
-  ResourceId: 'undefined',
-  Authors: [{
-    Name: 'undefined',
-    OrganizationName: 'undefined',
-    ContactInformation: {
-      Phone: 'undefined',
-      email: 'undefined',
-      Address: {
-        Street: 'undefined',
-        City: 'undefined',
-        State: 'undefined',
-        Zip: 'undefined'
-      }
+'cmd:resourceDescription': {
+  'cmd:resourceTitle': {type: String},
+  'cmd:resourceDescription': {type: String},
+  'cmd:resourceURI': [{
+    'cmd:citationIdentifier': {type: String},
+    'cmd:scopedIdentifierAuthority': {
+      'cmd:authorityURI': {type: String},
+      'cmd:authorityLabel': {type: String}
     }
   }],
-  Keywords: [],
-  GeographicExtent: {
-    NorthBound: 'undefined',
-    SouthBound: 'undefined',
-    EastBound: 'undefined',
-    WestBound: 'undefined'
+  'cmd:citedSourceAgents': {
+    'cmd:relatedAgent': {
+      'cmd:agentRole': {
+        'cmd:agentRoleURI': {type: String},
+        'cmd:agentRoleLabel': {type: String},
+        'cmd:individual': {
+          'cmd:personURI': {type: String},
+          'cmd:personName': {type: String},
+          'cmd:personPosition': {type: String}
+        },
+        'cmd:organizationName': [],
+        'cmd:organizationURI': {type: String},
+        'cmd:phoneNumber': {type: String},
+        'cmd:contactEmail': {type: String},
+        'cmd:contactAddress': {type: String}
+      }
+    }
   },
-  Links: [ngdsRecord objects should go here],
-  Distributors: [{
-    Name: 'undefined',
-    OrganizationName: 'undefined',
-    ContactInformation: {
-      Phone: 'undefined',
-      email: 'undefined',
-      Address: {
-        Street: 'undefined',
-        City: 'undefined',
-        State: 'undefined',
-        Zip: 'undefined'
+  'cmd:citationDates': {
+    'cmd:EventDateObject': {
+      'cmd:eventType': [{
+        'cmd:dateTypeLabel': {type: String},
+        'cmd:dateTypeURI': {type: String},
+        'cmd:dateTypeVocabularyURI': {type: String}
+      }],
+      'cmd:dateTime': {type: String}
+    }
+  },
+  'cmd:recommendedCitation': {type: String},
+  'cmd:resourceContacts': {
+    'cmd:relatedAgent': {
+      'cmd:agentRole': {
+        'cmd:agentRoleURI': {type: String},
+        'cmd:agentRoleLabel': {type: String},
+        'cmd:individual': {
+          'cmd:personURI': {type: String},
+          'cmd:personName': {type: String},
+          'cmd:personPosition': {type: String}
+        },
+        'cmd:organizationName': [],
+        'cmd:organizationURI': {type: String},
+        'cmd:phoneNumber': {type: String},
+        'cmd:contactEmail': {type: String},
+        'cmd:contactAddress': {type: String}
       }
     }
+  },
+  'cmd:resourceAccessOptions': [{
+    'cmd:distributor': {
+      'cmd:relatedAgent': {
+        'cmd:agentRole': {
+          'cmd:agentRoleURI': {type: String},
+          'cmd:agentRoleLabel': {type: String},
+          'cmd:individual': {
+            'cmd:personURI': {type: String},
+            'cmd:personName': {type: String},
+            'cmd:personPosition': {type: String}
+          },
+          'cmd:organizationName': [],
+          'cmd:organizationURI': {type: String},
+          'cmd:phoneNumber': {type: String},
+          'cmd:contactEmail': {type: String},
+          'cmd:contactAddress': {type: String}
+        }
+      }
+    },
+    'cmd:accessLinks': [{
+      'cmd:LinkObject': {
+        'cmd:url': {type: String},
+        'cmd:linkRelation': [{
+          'cmd:relLabel': {type: String},
+          'cmd:relURI': {type: String}
+        }],
+        'cmd:linkTitle': {type: String},
+        'cmd:linkTargetResourceType': {type: String},
+        'cmd:linkContentResourceType': {type: String},
+        'cmd:linkOverlayAPI': {
+          'cmd:APILabel': {type: String},
+          'cmd:API-URI': {type: String}
+        },
+        'cmd:linkProfile': {
+          'cmd:profileLabel': {type: String},
+          'cmd:profileURI': {type: String}
+        },
+        'cmd:linkParameters': [{
+          'cmd:linkParameterLabel': {type: String},
+          'cmd:linkParameterURI': {type: String},
+          'cmd:linkParameterValue': {type: String}
+        }],
+        'cmd:linkDescription': {type: String},
+        'cmd:linkTransferSize': {type: Number}
+      }
+    }]
   }],
-  MetadataContact: {
-    Name: 'undefined',
-    OrganizationName: 'undefined',
-    ContactInformation: {
-      Phone: 'undefined',
-      email: 'undefined',
-      Address: {
-        Street: 'undefined',
-        City: 'undefined',
-        State: 'undefined',
-        Zip: 'undefined'
+  'cmd:geographicExtent': {
+    'cmd:extentLabel': {type: String},
+    'cmd:extentStatement': {type: String},
+    'cmd:extentReference': {
+      'cmd:referencedExtentLabel': {type: String},
+      'cmd:referencedExtentURI': {type: String},
+      'cmd:extentVocabularyURI': {type: String}
+    },
+    'cmd:boundingBoxWGS84': [{
+      'cmd:northBoundLatitude': {type: Number, min: -90, max: 90},
+      'cmd:southBoundLatitude': {type: Number, min: -90, max: 90},
+      'cmd:eastBoundLongitude': {type: Number, min: -180, max: 180},
+      'cmd:westBoundLongitude': {type: Number, min: -180, max:180}
+    }],
+    'cmd:geoJSONgeometry': {
+      'cmd:extentGeometryType': {
+        'cmd:geometryTypeLabel': {type: String},
+        'cmd:geometryTypeURI': {type: String},
+        'cmd:geometryTypeVocabularyURI': {type: String}
+      },
+      'cmd:extentGeometry': [https://raw.githubusercontent.com/fge/sample-json-schemas/master/geojson/geojson.json]
+    },
+    'cmd:verticalExtent': {
+      'cmd:verticalExtentMinimum': {type: Number},
+      'cmd:verticalExtentMaximum': {type: Number},
+      'cmd:verticalExtentCRS': {
+        'cmd:verticalCRSLabel': {type: String},
+        'cmd:verticalCRS-URI': {type: String},
+        'cmd:verticalCRSVocabularyURI': {type: String}
       }
     }
-  }
+  },
+  'cmd:resourceTemporalExtent': [{
+    'temporalExtentBegin': {type: String},
+    'temporalExtentEnd': {type: String}
+  }],
+  'cmd:resourceUsageConstraints': [{
+    'cmd:constraintsStatement': {type: String},
+    'cmd:constraintTerms': [{
+      'cmd:constraintTypeLabel': {type: String},
+      'cmd:constraintTypeURI': {type: String},
+      'cmd:constraintTypeVocabularyURI': {type: String},
+      'cmd:constraintTerm': {type: String},
+      'cmd:constraintURI': {type: String},
+      'cmd:constraintVocabularyURI': {type: String}
+    }],
+    'cmd:license': [{
+      'cmd:licenseURI': {type: String},
+      'cmd:licenseName': {type: String}
+    }]
+  }],
+  'cmd:resourceBrowseGraphic': {
+    'cmd:url': {type: String},
+    'cmd:linkRelation': [#/definitions/Reference],
+    'cmd:linkTitle': {type: String},
+    'cmd:linkContentResourceType': {type: String}
+  },
+  'cmd:resourceLanguage': [#/definitions/Reference],
+  'cmd:resourceSpatialDescription': {
+    'cmd:resourceSpatialRepresentationType': {
+      'cmd:resourceSpatialRepresentationTypeLabel': {type: String},
+      'cmd:resourceSpatialRepresentationTypeURI': {type: String},
+      'cmd:resourceSpatialRepresentationTypeVocabularyURI': {type: String}
+    },
+    'cmd:resourceSpatialReferenceSystem': {
+      'cmd:resourceSpatialReferenceSystemLabel': {type: String},
+      'cmd:resourceSpatialReferenceSystemURI': {type: String},
+      'cmd:resourceSpatialReferenceSystemVocabularyURI': {type: String}
+    }
+  },
+  'cmd:relatedResources': [{
+    'cmd:LinkObject': {
+      'cmd:url': {type: String},
+      'cmd:linkRelation': [{
+        'cmd:relLabel': {type: String},
+        'cmd:relURI': {type: String}
+      }],
+      'cmd:linkTitle': {type: String},
+      'cmd:linkTargetResourceType': {type: String},
+      'cmd:linkContentResourceType': {type: String},
+      'cmd:linkOverlayAPI': {
+        'cmd:APILabel': {type: String},
+        'cmd:API-URI': {type: String}
+      },
+      'cmd:linkProfile': {
+        'cmd:profileLabel': {type: String},
+        'cmd:profileURI': {type: String}
+      },
+      'cmd:linkParameters': [{
+        'cmd:linkParameterLabel': {type: String},
+        'cmd:linkParameterURI': {type: String},
+        'cmd:linkParameterValue': {type: String}
+      }],
+      'cmd:linkDescription': {type: String},
+      'cmd:linkTransferSize': {type: Number}
+    }
+  }]
 }
-
-And here's what we want the ngds record schema to look like:
-ngdsRecord: {
-  URL: 'undefined',
-  Name: 'undefined',
-  Description: 'undefined',
-  Distributor: 'undefined',
-  ServiceType: 'undefined',
-  Layer: 'undefined',
-  ContentModelURI: 'undefined',
-  ContentModelVersion: 'undefined'
-},
 */
 
 ckan.module('ngds-contribute', function (jQuery, _) {
--- ckanext/ngds/metadata/fanstatic/ngds_contribute_form2.js
+++ ckanext/ngds/metadata/fanstatic/ngds_contribute_form2.js
@@ -37,10 +37,99 @@ ckan.module('metadata-contribute', function (jQuery, _) {
       })
     },
     buildSchema: function () {
+      var obj
+        , basic
+        , doc
+        , dateTime
+        , citedSourceAgents
+        , sourceAgents
+        , sourceAgent
+        , resourceContact
+        , distributors
+        , distribs
+        , distributor
+        , geo
+        , i
+        , j
+        ;
 
+      function buildRelatedAgent (section) {
+        var agent
+          , role
+          ;
+
+        agent = {};
+        agent.relatedAgent = {};
+        role = agent.relatedAgent.agentRole = {};
+        role.individual = {};
+
+        $(section).find('input').each(function () {
+          role.individual.personName = obj.processInputs(this, 'md-person-name');
+          role.individual.personPosition = obj.processInputs(this, 'md-person-position');
+          role.organizationName = obj.processInputs(this, 'md-organization-name');
+          role.phoneNumber = obj.processInputs(this, 'md-phone-number');
+          role.contactEmail = obj.processInputs(this, 'md-contact-email');
+          role.contactAddress = obj.processInputs(this, 'md-contact-address');
+        });
+
+        return agent;
+      }
+
+      obj = this;
+
+      basic = $('#collapse-basic-fields .ngds-input-form');
+      citedSourceAgents = $('#collapse-ngds-author-fields .ngds-input-form');
+      resourceContact = $('#collapse-ngds-metadata-contact-fields .ngds-input-form');
+      distributors = $('#collapse-ngds-distributor-fields .ngds-input-form');
+      geo = $('#collapse-ngds-geographic-extent-fields .ngds-input-form');
+
+      doc = {};
+      doc.citationDates = {};
+      doc.citationDates.EventDateObject = {};
+      dateTime = doc.citationDates.EventDateObject.dateTime = {};
+      basic.find('input').each(function () {
+        if ($(this).attr('name') === 'title') {
+          doc.resourceTitle = obj.processInputs(this, 'title');
+        }
+        doc.resourceDescription = obj.processInputs(this, 'description');
+        dateTime = obj.processInputs(this, 'publication_date');
+      });
+
+      sourceAgents = [];
+      for (i = 0; i < citedSourceAgents.length; i++) {
+        sourceAgent = citedSourceAgents[i];
+        sourceAgents.push(buildRelatedAgent(sourceAgent));
+      }
+      doc.citedSourceAgents = sourceAgents;
+
+      doc.resourceContact = buildRelatedAgent(resourceContact);
+
+      distribs = [];
+      for (j = 0; j < distributors.length; j++) {
+        distributor = distributors[j];
+        distribs.push(distributor);
+      }
+      doc.resourceAccessOptions = {};
+      doc.resourceAccessOptions.distributors = distribs;
+
+      doc.geographicExtent = [{
+        northBoundLatitude: obj.processInputs(geo, 'md-geo-north'),
+        southBoundLatitude: obj.processInputs(geo, 'md-geo-south'),
+        eastBoundLongitude: obj.processInputs(geo, 'md-geo-east'),
+        westBoundLongitude: obj.processInputs(geo, 'md-geo-west')
+      }];
+
+      return doc;
     },
     processInputs: function (input, search, defVal) {
-
+      if ($(input).attr('name') === search) {
+        return $(input).val();
+      } else {
+        if (!defVal) {
+          return 'undefined';
+        }
+        return defVal;
+      }
     }
   }
 });
