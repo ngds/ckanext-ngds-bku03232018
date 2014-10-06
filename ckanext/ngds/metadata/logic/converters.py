62f5ca0815f4c93386d78ffdb2ac31b358ea0bbc
asonnenschein <adrian.sonnenschein@gmail.com>
2014-10-05 19:05:19 -0700

    moved ckanext-ngds/metadata to ckanext-metadata and ckanext-usginpusher

--- ckanext/ngds/metadata/logic/converters.py
+++
@@ -1,5 +0,0 @@
-def convert_to_ngds_package_extras(key, data, errors, context):
-    extras = data.get(('extras',), [])
-    if not extras:
-        data[('extras',)] = extras
-    extras.append({'key': key[-1], 'value': data[key]})
