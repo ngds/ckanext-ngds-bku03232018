aefdda9bf3509c7d1d662ea363e6ee21f41bcb72
Jihad MOTII <jihad.motii@reisystems.com>
2014-12-24 05:56:02 +0900

    Bugfix #580 #596: Secure access to geoserver using proxy

--- ckanext/ngds/client/logic/action.py
+++ ckanext/ngds/client/logic/action.py
@@ -3,6 +3,8 @@ from ckanext.ngds.common import logic
 from ckanext.ngds.client.model import ogc
 
 import urllib
+from pylons import config
+from urlparse import urlparse
 
 def geothermal_prospector_url(context, data_dict):
     try:
@@ -11,8 +13,23 @@ def geothermal_prospector_url(context, data_dict):
         gtp_layer = '6'
         wms = ogc.HandleWMS(search['url'])
         wms_info = wms.get_layer_info(search)
+	service_url = wms_info['service_url']
+
+	#PROXY SETTING
+        #If geoserver is under proxy, change service_url to proxied one in order to show wms layer by JS
+        useProxy = config.get("geoserver.use_proxy", False)
+        ProxiedPath = config.get("geoserver.proxied_path", None)
+        siteUrl = config.get("ckan.site_url", "http://127.0.0.1")
+
+        if useProxy and ProxiedPath and siteUrl:
+            urlParsed = urlparse(service_url)
+            #Generate proxied geoserver link (path.replace: it replaces original goeserver path with proxied path)
+            service_url = siteUrl+urlParsed.path.replace('/geoserver', ProxiedPath)+'?'+urlParsed.query
+
+        #END PROXY SETTING
+
         url = gtp_url + '?baselayer=' + gtp_layer + '&zoomlevel=3&wmsHost=' \
-              + urllib.quote(wms_info['service_url'], '') + '&wmsLayerName=' \
+              + urllib.quote(service_url, '') + '&wmsLayerName=' \
               + wms_info['layer']
         return url
     except:
